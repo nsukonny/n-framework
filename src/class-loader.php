827c265cd29ab87884e9531e90c22a5fd2033d0b
Nickolay Sukonny <kajenick@gmail.com>
2021-11-03 07:29:02 +0200

    fix loader

--- src/class-loader.php
+++ src/class-loader.php
@@ -5,50 +5,56 @@
 
 namespace NSukonny\NFramework;
 
-function autoload( $class ) {
-	echo $class . ' ----- <br>';
-	global $nhg_autoload_namespaces;
+class Loader {
 
-	echo '<pre>' . print_r( $class, true ) . '</pre>';
-	if ( strpos( $class, 'NHG\\' ) !== 0 || empty( $nhg_autoload_namespaces ) ) {
-		return;
+	public function init() {
+		//spl_autoload_register( __NAMESPACE__ . '::autoload' );
 	}
 
-	$load_path = null;
-	$autoload  = false;
+	public static function autoload( $class ) {
+		echo $class . ' ----- <br>';
+		global $nhg_autoload_namespaces;
 
-	$pieces    = explode( '\\', $class );
-	$classname = array_pop( $pieces );
-	$namespace = implode( '\\', $pieces );
-
-	foreach ( $nhg_autoload_namespaces as $key => $load_path ) {
-		if ( $namespace === $key || ( strpos( $namespace, $key . '\\' ) === 0 ) ) {
-			$autoload = true;
-			break;
-		}
-	}
-
-	if ( ! $autoload || ! $load_path ) {
-		return;
-	}
-
-	$path = $load_path . '/includes' . strtolower( str_replace( [ '\\', '_' ], [
-			'/',
-			'-'
-		], substr( $namespace, strlen( $key ) ) ) ) . '/';
-	$slug = strtolower( str_replace( '_', '-', $classname ) ) . '.php';
-
-	$prefixes = [ 'class', 'trait', 'abstract' ];
-
-	foreach ( $prefixes as $prefix ) {
-		$filename = $path . $prefix . '-' . $slug;
-
-		if ( file_exists( $filename ) ) {
-			require_once $filename;
-
+		echo '<pre>' . print_r( $class, true ) . '</pre>';
+		if ( strpos( $class, 'NHG\\' ) !== 0 || empty( $nhg_autoload_namespaces ) ) {
 			return;
 		}
-	}
-}
 
-spl_autoload_register( __NAMESPACE__ . '::autoload' );
+		$load_path = null;
+		$autoload  = false;
+
+		$pieces    = explode( '\\', $class );
+		$classname = array_pop( $pieces );
+		$namespace = implode( '\\', $pieces );
+
+		foreach ( $nhg_autoload_namespaces as $key => $load_path ) {
+			if ( $namespace === $key || ( strpos( $namespace, $key . '\\' ) === 0 ) ) {
+				$autoload = true;
+				break;
+			}
+		}
+
+		if ( ! $autoload || ! $load_path ) {
+			return;
+		}
+
+		$path = $load_path . '/includes' . strtolower( str_replace( [ '\\', '_' ], [
+				'/',
+				'-'
+			], substr( $namespace, strlen( $key ) ) ) ) . '/';
+		$slug = strtolower( str_replace( '_', '-', $classname ) ) . '.php';
+
+		$prefixes = [ 'class', 'trait', 'abstract' ];
+
+		foreach ( $prefixes as $prefix ) {
+			$filename = $path . $prefix . '-' . $slug;
+
+			if ( file_exists( $filename ) ) {
+				require_once $filename;
+
+				return;
+			}
+		}
+	}
+
+}
