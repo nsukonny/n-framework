34ec27dfdf06b136c022ba8aa3ae590afd4af890
Nickolay Sukonny <kajenick@gmail.com>
2021-11-03 06:09:30 +0200

    Update keywords

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'b016dbcca39b77081b34a64b3b079b0e4ffd6e27',
+        'name' => 'nsukonny/n-framework',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'nsukonny/n-framework' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'b016dbcca39b77081b34a64b3b079b0e4ffd6e27',
+            'dev_requirement' => false,
+        ),
+    ),
+);
